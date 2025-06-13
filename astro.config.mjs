ac1ddc21acf1db5fd814c30719c7d42278e381a2
zelpro-dev <brawlstarszelpro@gmail.com>
2025-06-13 22:02:03 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: "https://zelpro.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
